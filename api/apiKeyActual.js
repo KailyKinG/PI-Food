bb8b5613d5fed48b8acdc52ae9a08e399b6783af
KailyKinG <kailyking@gmail.com>
2023-03-02 00:36:29 -0300

    Se Cambio la ApiKey

--- api/apiKeyActual.js
+++ api/apiKeyActual.js
@@ -9,26 +9,10 @@ const {
   API_KEY2, 
   API_KEY3,
   API_KEY4,
-  API_KEY5,//* no funciona 401
-  API_KEY6, //* no funciona 401
-  API_KEY7, 
-  API_KEY8, 
-  API_KEY9,
-  API_KEY10,
-  API_KEY11,
-  API_KEY12,
-  API_KEY13,
-  API_KEY14,
-  API_KEY15,
-  API_KEY_F,
-  API_KEY16,
-  API_KEY17,
-  API_KEY18,
-  API_KEY19,
-  API_KEY20 } = process.env;
+  API_KEY5 } = process.env;
 
 //Si Nos Quedamos Sin Request, Le Damos Otra API_KEY A Nuestra Variable 'clave'
-const clave = API_KEY17;
+const clave = API_KEY2;
 
 // El Cambio Se Realiza En Un Solo Lugar
 // Quedando Mas Limpio Nuestro Codigo.
