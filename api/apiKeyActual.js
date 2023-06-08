f4c336ed01e02e4371119131b67bbba98e662387
KailyKinG <kailyking@gmail.com>
2023-06-08 16:58:11 -0400

    Se cambio la ApkiKey

--- api/apiKeyActual.js
+++ api/apiKeyActual.js
@@ -1,18 +1,12 @@
-
 /**
  * Creamos Un Solo Archivo En Donde Importaremos Nuestras API_KEYS Desde
    process.env, Declaramos Una Variable Constante, Llamada 'clave', Esta 
    La Dejaremos A Dispocision De Quien la Necesite Exportandola
  */
-const { 
-  API_KEY,
-  API_KEY2, 
-  API_KEY3,
-  API_KEY4,
-  API_KEY5 } = process.env;
+const { API_KEY, API_KEY2, API_KEY3, API_KEY4, API_KEY5 } = process.env;
 
 //Si Nos Quedamos Sin Request, Le Damos Otra API_KEY A Nuestra Variable 'clave'
-const clave = API_KEY3;
+const clave = API_KEY4;
 
 // El Cambio Se Realiza En Un Solo Lugar
 // Quedando Mas Limpio Nuestro Codigo.
