af5dfbd81cf35364f086adf8296365e7614d72d6
KailyKinG <kailyking@gmail.com>
2023-06-08 20:10:16 -0400

    agregue el servicio backend de 'Render'. Lo deje por defecto

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -25,7 +25,8 @@ import {
 // BASE DE LA URL QUE VIENE DESDE BACKEND
 // ---------------------------------------
 // const baseURL = "http://localhost:3001";
-const baseURL = "https://pi-food-production-5535.up.railway.app";
+// const baseURL = "https://pi-food-production-5535.up.railway.app";
+const baseURL = "https://pi-food-server.onrender.com";
 // ------------------------------------------------------------------
 
 //CREAMOS NUESTRAS ACTIONS
